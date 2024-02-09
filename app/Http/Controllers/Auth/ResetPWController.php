427ad6e7dc681d9266147ffd20780a3bd5961046
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2024-02-09 17:19:03 +0100

    ResetPWController

---
+++ app/Http/Controllers/Auth/ResetPWController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Str;
+
+class ResetPWController extends Controller
+{
+    public function index($token): \Illuminate\Contracts\View\Factory|\Illuminate\Foundation\Application|\Illuminate\Contracts\View\View|\Illuminate\Contracts\Foundation\Application
+    {
+        $email = request()->input('email');
+
+        return view('auth.reset_pw', compact('email', 'token'));
+    }
+
+
+    public function reset_pw(Request $request): \Illuminate\Http\RedirectResponse
+    {
+        $request->validate([
+            'token' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|string|confirmed|min:8',
+        ]);
+
+        $status = Password::reset(
+            $request->only('email', 'password', 'password_confirmation', 'token'),
+            function ($user) use ($request) {
+                $user->forceFill([
+                    'password' => Hash::make($request->password),
+                    'remember_token' => Str::random(60)
+                ])->save();
+            }
+        );
+
+        if ($status == Password::PASSWORD_RESET) {
+            Log::info('Password reset successful for email: ' . $request->email);
+            return redirect()->route('login')->with('status', __($status));
+        } else {
+            Log::error('Password reset failed: ' . $status);
+            return back()->withInput($request->only('email'))->withErrors(['email' => __($status)]);
+        }
+    }
+}
