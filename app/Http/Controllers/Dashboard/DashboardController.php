7386eb5bf3492d2f86b909960b8230b8da099ddc
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2024-02-12 22:48:35 +0100

    update dash controller

--- app/Http/Controllers/Dashboard/DashboardController.php
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
+use App\Models\Member;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    public function index()
+    public function index(): \Illuminate\Contracts\View\Factory|\Illuminate\Foundation\Application|\Illuminate\Contracts\View\View|\Illuminate\Contracts\Foundation\Application
     {
         $members = Member::all();
         return view('dashboard.index', ['members' => $members]);
