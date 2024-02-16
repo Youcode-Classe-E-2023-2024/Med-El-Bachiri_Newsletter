86ff1f6d808535c8cb8f71f3a5e195b05b47f973
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2024-02-16 18:10:03 +0100

    send mail to subs class mail

---
+++ app/Mail/SendMailToSubs.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Media;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Storage;
+
+class SendMailToSubs extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $template;
+    public $file_name;
+    public $file_id;
+
+    public function __construct($template, $file_name, $file_id)
+    {
+        $this->template = $template;
+        $this->file_name = $file_name;
+        $this->file_id = $file_id;
+    }
+
+
+    public function build()
+    {
+        $filePath = Storage::path('public/' . $this->file_id . '/' . $this->file_name);
+        return $this->from('mohammedelbachiri945@gmail.com', 'News Letter Bachiri')
+            ->html(htmlspecialchars_decode($this->template))
+            ->attach($filePath);
+    }
+}
